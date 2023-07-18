43c6cf7bc5d34864eb0299a4266e35f764d475b4
power <1410837981@qq.com>
2023-07-18 08:37:33 +0800

    feat:增加证书

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="fixed top-0 w-full bg-black bg-opacity-30 z-1000"
+    :class="{ 'bg-black bg-opacity-50 shadow-lg': y > 0 }"
+  >
+    <Container>
+      <Menu></Menu>
+    </Container>
+  </div>
+</template>
+<script setup lang="ts" generic="T"></script>
+<route lang="yaml">
+meta:
+  layout: default
+</route>
---
+++ uno.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'unocss'
+import { presetWind, presetIcons, transformerDirectives } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetWind(),
+
+    presetIcons({
+      prefix: 'i-',
+      extraProperties: {
+        display: 'inline-block'
+      }
+    })
+  ],
+  transformers: [transformerDirectives()]
+})
